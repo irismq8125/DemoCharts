c8d220a3c44dacb0581518be2b31035bd4a7e44d
hv <hv@PM307-GV>
2024-01-08 20:39:14 +0700

    Add project files.

---
+++ DemoCharts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoCharts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
